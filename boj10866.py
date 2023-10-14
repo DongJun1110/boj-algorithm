ebc11c1cac4cc6edc24c19053dcfbb3fc82588f2
cckim <r2222911r@ajou.ac.kr>
2023-10-14 13:02:35 +0900

    덱

---
+++ boj10866.py
@@ -0,0 +1,35 @@
+import sys
+
+dq = []
+
+for _ in range(int(sys.stdin.readline())):
+  command = list(sys.stdin.readline().split())
+  if command[0] == 'push_front':
+    dq.append(command[1])
+    
+  elif command[0] == 'push_back':
+    dq.reverse()
+    dq.append(command[1])
+    dq.reverse()
+    
+  elif command[0] == 'pop_front':
+    if dq: print(dq.pop())
+    else: print(-1)
+
+  elif command[0] == 'pop_back':
+    if dq: print(dq.pop(0))
+    else: print(-1)
+
+  elif command[0] == 'size':
+    print(len(dq))
+
+  elif command[0] == 'empty':
+    print(0) if len(dq) != 0 else print(1)
+
+  elif command[0] == 'front':
+    print(dq[len(dq)-1]) if len(dq) != 0 else print(-1)
+
+  elif command[0] == 'back':
+    print(dq[0]) if len(dq) != 0 else print(-1)
+
+
