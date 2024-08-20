807dfb910e6586c49f2e408dc5940533c1df1575
kimdongjun <r2222911r@ajou.ac.kr>
2024-08-20 10:50:53 +0900

    연구소

---
+++ boj14502.py
@@ -0,0 +1,57 @@
+from collections import deque
+import copy
+
+n,m = map(int,input().split())
+graph = []
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+def bfs():
+    q = deque()
+    tmp_graph = copy.deepcopy(graph)
+    
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 2:
+                q.append((i,j))
+                
+    while q:
+        x,y = q.popleft()
+        
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if tmp_graph[nx][ny] == 0:
+                tmp_graph[nx][ny] = 2
+                q.append((nx,ny))
+    
+    global answer
+    cnt = 0
+    
+    for i in range(n):
+        cnt += tmp_graph[i].count(0)
+        
+    answer = max(answer, cnt)
+  
+
+def makewall(cnt):
+    if cnt == 3:
+        bfs()
+        return
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                makewall(cnt+1)
+                graph[i][j] = 0
+                
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+    
+answer = 0
+makewall(0)
+print(answer)
+   
